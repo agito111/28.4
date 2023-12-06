7ea7db64e25aafbd03fcdbf310dc6ad417a40e82
agito111 <a.tsanevaa@gmail.com>
2023-12-06 05:26:16 +0100

    Add project files.

---
+++ 28.4/Documento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+enum Stato {Disponibile, InPrestito }
+
+namespace _28._4
+{
+    internal class Documento
+    {
+        public string Codice { get; set; }
+        public string Titolo { get; set; }
+        public int Anno { get; set; }
+        public string Settore { get; set; }
+        public Stato Stato { get; set; }
+        public  Scaffale Scaffale { get; set; }
+        public List<Autore> Autore { get; set; }
+    }
+}
---
+++ 28.4/Prestito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._4
+{
+    internal class Prestito
+    {
+        public string Numero { get; set; }
+        public DateTime Dal { get; set; }
+        public DateTime Al { get; set;}
+    }
+}
